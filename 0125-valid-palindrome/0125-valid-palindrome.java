9a5d78e81f5430a7f25d01014b91c9e20cf9c22b
guruuu121-creator <guruuu121@gmail.com>
2026-06-23 07:48:26 +0530

    Time: 137 ms (5.08%), Space: 48.3 MB (5.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,13 @@
- class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-             }
-                while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
-                }
-                while(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-                    return false;
-                }
-            left++;
-            right--;
-     }
-     return true;
+       s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+       String rev = "";
+       for(int i=s.length()-1;i>=0;i--){
+        rev = rev + s.charAt(i);
+       }if(s.equals(rev)){
+        return true;
+       }else{
+        return false;
+       }
     }
 }
